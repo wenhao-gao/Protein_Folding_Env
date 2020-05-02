f16412329e56becfdb8980ce68d29150e67e44f7
Wenhao Gao <gaowh19@gmail.com>
2020-05-02 01:10:12 -0400

    argument type

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-
-CUDA_VISIBLE_DEVICES= nohup python -u run_ppo.py \
+TASK=`date +"%Y%m%d_%T"`_1qgm_nmc
+CUDA_VISIBLE_DEVICES=0 nohup python -u run_ppo_nmc.py \
     -c 'agents/configs/ppo.json' \
-    --task '1qgm_test' \
+    --task ${TASK} \
     --log_frequency 1 \
-    --ref_pdb 'data/protein_folding/1qgm.pdb' &> temp.out&
+    --ref_pdb 'data/protein_folding/1qgm.pdb' &> ${TASK}.out&
--- utilities/parsing.py
+++ utilities/parsing.py
@@ -22,13 +22,13 @@ def add_args(parser: ArgumentParser):
                         help='path to put output files.')
     parser.add_argument('-o', '--gen_path', default='./results',
                         help='The file to store results.')
-    parser.add_argument('-n', '--number', default=100,
+    parser.add_argument('-n', '--number', default=100, type=int,
                         help='Number of molecules to keep')
     parser.add_argument('-v', '--verbose', action='store_true', default=True,
                         help='Whether to see the intermediate information.')
-    parser.add_argument('--save_frequency', default=1000,
+    parser.add_argument('--save_frequency', default=1000, type=int,
                         help='The frequency to save a checkpoint.')
-    parser.add_argument('--log_frequency', default=100,
+    parser.add_argument('--log_frequency', default=100, type=int,
                         help='The frequency to log result.')
 
     # Protein environment arguments
@@ -40,29 +40,29 @@ def add_args(parser: ArgumentParser):
                         help='The path to the fastq file')
     parser.add_argument('--loop', default=None,
                         help='The path to the loop file')
-    parser.add_argument('--max_steps', default=100000000,
+    parser.add_argument('--max_steps', default=100000000, type=int,
                         help='Maximum steps allowed.')
-    parser.add_argument('--discount_factor', default=1,
+    parser.add_argument('--discount_factor', default=1, type=float,
                         help='The discount factor of reward.')
 
     # Reinforcement Learning arguments
-    parser.add_argument('--num_episodes', default=10,
+    parser.add_argument('--num_episodes', default=10, type=int,
                         help='Number of episodes to run.')
-    parser.add_argument('--replay_buffer_size', default=5000,
+    parser.add_argument('--replay_buffer_size', default=5000, type=int,
                         help='The action reward sets to store in the replay buffer.')
     parser.add_argument('--exploration', default='bootstrap',
                         choices=['bootstrap', 'disagreement'],
                         help='Choose the exploration method.')
     # PPO
-    parser.add_argument('--batch_size', default=10,
+    parser.add_argument('--batch_size', default=10, type=int,
                         help='The training batch size.')
-    parser.add_argument('--gamma', default=0.99,
+    parser.add_argument('--gamma', default=0.99, type=float,
                         help='The normally defined discount factor.')
-    parser.add_argument('--tau', default=0.95,
+    parser.add_argument('--tau', default=0.95, type=float,
                         help='The GAE discount factor.')
-    parser.add_argument('--clip_param', default=0.2,
+    parser.add_argument('--clip_param', default=0.2, type=float,
                         help='The PPO clip factor.')
-    parser.add_argument('--update_frequency', default=20,
+    parser.add_argument('--update_frequency', default=20, type=int,
                         help='The network update frequency.')
 
     parser.add_argument('--activation', type=str, default='ReLU',
@@ -72,39 +72,39 @@ def add_args(parser: ArgumentParser):
                         help='The opitmizer to use.')
     parser.add_argument('--batch_norm', action='store_true', default=False,
                         help='Use the batch normalization or not.')
-    parser.add_argument('--dropout', default=0,
+    parser.add_argument('--dropout', default=0, type=float,
                         help='The dropout probability.')
-    parser.add_argument('--adam_beta_1', default=0.9,
+    parser.add_argument('--adam_beta_1', default=0.9, type=float,
                         help='The beta_1 in adam optimizer.')
-    parser.add_argument('--adam_beta_2', default=0.999,
+    parser.add_argument('--adam_beta_2', default=0.999, type=float,
                         help='The beta_2 in adam optimizer.')
-    parser.add_argument('--grad_clipping', default=10,
+    parser.add_argument('--grad_clipping', default=10, type=float,
                         help='The gradient clipping.')
-    parser.add_argument('--learning_frequency', default=4,
+    parser.add_argument('--learning_frequency', default=4, type=int,
                         help='The frequency of learning, steps')
-    parser.add_argument('--learning_rate', default=1e-4,
+    parser.add_argument('--learning_rate', default=1e-4, type=float,
                         help='The learning rate to begin with.')
     parser.add_argument('--learning_rate_decay_steps', default=1000,
                         help='Learning rate decay steps.')
-    parser.add_argument('--learning_rate_decay_rate', default=0.8,
+    parser.add_argument('--learning_rate_decay_rate', default=0.8, type=float,
                         help='Learning rate decay rate.')
 
     # Graph network part
-    parser.add_argument('--node_dim', default=40,
+    parser.add_argument('--node_dim', default=40, type=int,
                         help='The dimension of node feature vector. 40 for low-resolution, 49 for high-resolution. Default is 40.')
-    parser.add_argument('--edge_dim', default=23,
+    parser.add_argument('--edge_dim', default=23, type=int,
                         help='The dimension of edge feature vector. 23 for low-resolution, 32 for high-resolution. Default is 23.')
-    parser.add_argument('--hidden_dim', default=64,
+    parser.add_argument('--hidden_dim', default=64, type=int,
                         help='The dimension of hidden vector.')
     parser.add_argument('--bias', action='store_true', default=False,
                         help='To use bias in graph network or not.')
-    parser.add_argument('--processing_steps', default=3,
+    parser.add_argument('--processing_steps', default=3, type=int,
                         help='The depth of set2set self-attention layer.')
-    parser.add_argument('--depth', default=3,
+    parser.add_argument('--depth', default=3, type=int,
                         help='The message passing depth of graph network.')
-    parser.add_argument('--ffn_hidden_size', default=300,
+    parser.add_argument('--ffn_hidden_size', default=300, type=int,
                         help='The hidden size of following ffn.')
-    parser.add_argument('--ffn_num_layers', default=2,
+    parser.add_argument('--ffn_num_layers', default=2, type=int,
                         help='The number of layers of following ffn.')
     parser.add_argument('--atom_messages', action='store_true', default=False,
                         help='To use atom features instead of concatenation of atom and bond')
